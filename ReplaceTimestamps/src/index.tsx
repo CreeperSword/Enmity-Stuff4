ed0e765d683d42bfa8b6e5bc332ab2330a054665
CreeperSword <93549710+CreeperSword@users.noreply.github.com>
2024-04-07 18:51:08 -0400

    Update index.tsx

--- ReplaceTimestamps/src/index.tsx
+++ ReplaceTimestamps/src/index.tsx
@@ -21,7 +21,7 @@ const ReplaceTimestamps: Plugin = {
         };
 
         Patcher.before(Messages, "sendMessage", (_, [, msg]) => {
-            const REGEX = /\b(0?[0-9]|1[0-9]|2[0-4]):([0-5][0-9])( ?[ap]m)?\b/gi;
+            const REGEX = /\b(0?[0-9]|1[0-9]|2[0-4]);([0-5][0-9])( ?[ap]m)?\b/gi;
             if (msg.content.search(REGEX) !== -1)
                 msg.content = msg.content.replace(REGEX, (x: string) => {
                     let hours: number, minutes: string, mode: null | "AM" | "PM";
